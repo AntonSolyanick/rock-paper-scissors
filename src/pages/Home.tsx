af7220ce6819cd7d991eaccfddb97b9a7d195290
Anton <antonsolyanick@gmail.com>
2024-06-08 00:10:38 +0300

    fixing overflow styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ const Home: FC = () => {
       animate={{ opacity: 1, transition: { duration: 1 } }}
       exit={{ opacity: 0, transition: { duration: 0.3 } }}
     >
-      <div className="container--buttons">
+      <section className="container--buttons">
         {pickOtions.map((option, i) => {
           const style = { "--i": i } as React.CSSProperties;
           return (
@@ -42,7 +42,7 @@ const Home: FC = () => {
           );
         })}
         <span className={styles.pentagon}>{pentagon}</span>
-      </div>
+      </section>
     </motion.main>
   );
 };
